20099801af3fdc9211c63dbf727a37d72a88e018
michaelbyrd <michaelbyrdsemail@gmail.com>
2014-07-09 14:14:36 -0400

    installed simpleform --foundation, added topbar to apllication, created the views and forms for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,30 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update]
 
 def index
+  @posts = Post.all
 end
 
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+  if @post.save
+    redirect_to @post, notice: "Your post was added."
+  else
+    render :new
+  end
+end
+
+private
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content )
+  end
 
 end
